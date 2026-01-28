a0f47690add1fb7263b368b1a6ec04a76bef0ed7
AnatoleLucet <anatole.lucet@proton.me>
2026-01-28 12:38:46 +0100

    perf: small optim for measure funcs

--- measure.go
+++ measure.go
@@ -27,27 +27,20 @@ import "runtime/cgo"
 
 type MeasureFunc func(node *Node, width float32, widthMode MeasureMode, height float32, heightMode MeasureMode) Size
 
+type measureContext struct {
+	node *Node
+	fn   MeasureFunc
+}
+
 //export goMeasureCallback
-func goMeasureCallback(node C.YGNodeConstRef, width C.float, widthMode C.YGMeasureMode, height C.float, heightMode C.YGMeasureMode) (result C.YGSize) {
-	ctx := C.tessGetNodeContext(C.YGNodeRef(node))
-	if ctx == 0 {
+func goMeasureCallback(node C.YGNodeConstRef, width C.float, widthMode C.YGMeasureMode, height C.float, heightMode C.YGMeasureMode) C.YGSize {
+	ctxPtr := C.tessGetNodeContext(C.YGNodeRef(node))
+	if ctxPtr == 0 {
 		return C.YGSize{width: 0, height: 0}
 	}
 
-	// Handle race condition where the handle may be deleted between
-	// checking ctx and calling handle.Value()
-	defer func() {
-		if r := recover(); r != nil {
-			result = C.YGSize{width: 0, height: 0}
-		}
-	}()
-
-	handle := cgo.Handle(ctx)
-	fn := handle.Value().(MeasureFunc)
-
-	// todo: that's unoptimized. We create a new Node wrapper every time.
-	goNode := newNode(getDefaultConfig(), C.YGNodeRef(node))
-	size := fn(goNode, float32(width), fromYGMeasureMode(widthMode), float32(height), fromYGMeasureMode(heightMode))
+	ctx := cgo.Handle(ctxPtr).Value().(*measureContext)
+	size := ctx.fn(ctx.node, float32(width), fromYGMeasureMode(widthMode), float32(height), fromYGMeasureMode(heightMode))
 
 	return C.YGSize{width: C.float(size.Width), height: C.float(size.Height)}
 }
@@ -60,7 +53,8 @@ func (n *Node) SetMeasureFunc(fn MeasureFunc) {
 	n.mu.Lock()
 	defer n.mu.Unlock()
 
-	handle := cgo.NewHandle(fn)
+	ctx := &measureContext{node: n, fn: fn}
+	handle := cgo.NewHandle(ctx)
 	C.tessSetNodeContext(n.node, C.uintptr_t(handle))
 	C.tessSetMeasureFunc(n.node)
 }
@@ -87,18 +81,18 @@ func (n *Node) UnsetMeasureFunc() {
 }
 
 func (n *Node) getMeasureFunc() MeasureFunc {
-	ctx := C.tessGetNodeContext(n.node)
-	if ctx == 0 {
+	ctxPtr := C.tessGetNodeContext(n.node)
+	if ctxPtr == 0 {
 		return nil
 	}
 
-	handle := cgo.Handle(ctx)
-	fn, ok := handle.Value().(MeasureFunc)
+	handle := cgo.Handle(ctxPtr)
+	ctx, ok := handle.Value().(*measureContext)
 	if !ok {
 		return nil
 	}
 
-	return fn
+	return ctx.fn
 }
 
 func (n *Node) reregisterMeasureFunc(fn MeasureFunc) {
@@ -109,7 +103,8 @@ func (n *Node) reregisterMeasureFunc(fn MeasureFunc) {
 		return
 	}
 
-	handle := cgo.NewHandle(fn)
+	ctx := &measureContext{node: n, fn: fn}
+	handle := cgo.NewHandle(ctx)
 	C.tessSetNodeContext(n.node, C.uintptr_t(handle))
 	C.tessSetMeasureFunc(n.node)
 }
